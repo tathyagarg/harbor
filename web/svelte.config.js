204e86b5d2d31f96d43380ee94dc1a82eff75914
Tathya <coding.tathya@gmail.com>
2026-03-09 21:30:53 +0530

    fix: Kill me

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,17 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(
-    ),
+    adapter: adapter({
+      fallback: 'index.html',
+    }),
     paths: {
-      base: dev ? '' : '/harbor',
+      base: process.argv.includes('dev') ? '' : '/harbor',
     }
   }
 };
