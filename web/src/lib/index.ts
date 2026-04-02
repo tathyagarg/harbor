5225d75260c6738809e9ada27ead716d601953d7
Tathya <coding.tathya@gmail.com>
2026-04-02 14:50:04 +0530

    refactor: Restarting website

--- web/src/lib/index.ts
+++
@@ -1,2 +0,0 @@
-export let primaryColor: string = "#a1cca5";
-export let primaryTextColor: string = "#000000";
--- web/src/routes/+layout.ts
+++
@@ -1,6 +0,0 @@
-import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-
-export const prerender = true;
-export const trailingSlash = 'always';
-
-injectSpeedInsights();
